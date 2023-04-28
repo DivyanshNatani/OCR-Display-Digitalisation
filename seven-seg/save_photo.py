22190a2818f1ce39d64f9b5188662de2eb293b57
Divyansh <divyansh.natani@gmail.com>
2023-04-28 08:47:58 +0530

    updated code

---
+++ seven-seg/save_photo.py
@@ -0,0 +1,12 @@
+import cv2
+
+import requests
+
+url = 'http://192.168.0.102:8080/photo.jpg'
+page = requests.get(url)
+
+f_ext = '_myimg'
+f_name = 'img{}.jpg'.format(f_ext)
+with open(f_name, 'wb') as f:
+    f.write(page.content)
+    print("Image Accessed")
