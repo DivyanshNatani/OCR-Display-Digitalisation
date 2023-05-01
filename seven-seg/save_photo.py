0f09aa11c78c4490941973b5f2888fbe5e65e9fd
Divyansh <divyansh.natani@gmail.com>
2023-05-01 11:13:32 +0530

    Additional changes

--- seven-seg/save_photo.py
+++ seven-seg/save_photo.py
@@ -6,7 +6,7 @@ url = 'http://192.168.0.102:8080/photo.jpg'
 page = requests.get(url)
 
 f_ext = '_myimg'
-f_name = 'img{}.jpg'.format(f_ext)
+f_name = 'saved_photos/img{}.jpg'.format(f_ext)
 with open(f_name, 'wb') as f:
     f.write(page.content)
     print("Image Accessed")
